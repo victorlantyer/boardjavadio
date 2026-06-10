4075a77a3d88a5f676b6767731f74b60a2ec348c
victorlantyer <110649955+victorlantyer@users.noreply.github.com>
2026-06-10 19:26:37 -0300

    Add board column info DTO

---
+++ src/main/java/br/com/dio/dto/BoardColumnInfoDTO.java
@@ -0,0 +1,12 @@
+package br.com.dio.dto;
+
+import br.com.dio.persistence.entity.BoardColumnKindEnum;
+
+public record BoardColumnInfoDTO(
+        Long id,
+        String name,
+        int order,
+        BoardColumnKindEnum kind,
+        int cardsAmount
+) {
+}
